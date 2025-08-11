a7740cbd9878b93d08255b7c8ad80772c1a66dc7
Edson Júnior <efojunior25@gmail.com>
2025-08-11 18:22:57 -0300

    initial commit

---
+++ src/test/java/com/xunim/javatraining01/JavaTraining01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xunim.javatraining01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTraining01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
